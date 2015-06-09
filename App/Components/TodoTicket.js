244ffb8abdd4a0836c5d295a8eeb40ebb94b4b1f
Josh Crowder <josh@seriousfox.co.uk>
2015-06-09 17:45:54 +0800

    add set state

--- App/Components/TodoTicket.js
+++ App/Components/TodoTicket.js
@@ -16,21 +16,37 @@ var {
 } = React;
 
 class TodoTicket extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      completed: false
+    }
+  }
+  handleTicketState() {
+    this.setState({completed: !this.state.completed})
+  }
   render() {
     var ticket = this.props.ticket;
+    var todoStyle = {
+      backgroundColor:'#DCDCDC'
+    }
+    var completeStyle = {
+      backgroundColor:'#B2D171'
+    }
+    var ticketStyle = this.state.ticketState ? completeStyle : todoStyle
     return (
       <View style={[ticketStyles.ticket,styles.ticket]}>
         <View style={[ticketStyles.leftContainer,styles.leftContainer]}>
           <View style={{alignItems:'center',}}>
             <TouchableHighlight
-              onPress={() => this.handleTicketState()}
+              onPress={this.handleTicketState.bind(this)}
               underlayColor={'#B2D171'}
-              style={{backgroundColor:'#DCDCDC',borderRadius: 4,}}>
+              style={[ticketStyle,{borderRadius: 4}]}>
               <Icon
                 name='fontawesome|check'
                 size={20}
                 color={'#F7F7F7'}
-                style={{width: 30,height: 30,borderRadius: 4,}}
+                style={{width: 30,height: 30,borderRadius: 4}}
               />
             </TouchableHighlight>
           </View>

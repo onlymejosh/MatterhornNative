279897f27750647be8c946034fb4bfeefdc1fb0a
Josh Crowder <josh@seriousfox.co.uk>
2015-06-12 18:13:49 +0800

    remove old api requests

--- App/Utils/api.js
+++ App/Utils/api.js
@@ -3,16 +3,6 @@ var URLS = {
 };
 
 var api = {
-  getBio(username){
-    username = username.toLowerCase().trim();
-    var url = `https://api.github.com/users/${username}`;
-    return fetch(url).then((res) => res.json())
-  },
-  getRepos(username){
-    username = username.toLowerCase().trim();
-    var url = `https://api.github.com/users/${username}/repos`;
-    return fetch(url).then((res) => res.json());
-  },
   getProjects(authentication) {
     console.log('Auth',authentication);
     return fetch(URLS.projects,{

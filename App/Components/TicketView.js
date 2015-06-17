0384e0ca5633bd3181b5b65123d5e97b40a54137
Josh Crowder <josh@seriousfox.co.uk>
2015-06-17 11:10:40 +0800

    Adding layout

--- App/Components/TicketView.js
+++ App/Components/TicketView.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var React = require('react-native');
-
+var Icon = require('FAKIconImage');
 var Header = require('./Header');
 var styles = require('../Styles/TicketView');
 
@@ -22,10 +22,79 @@ class TicketView extends Component {
   }
 
   render() {
+    var ticket = this.props.route.passProps.ticket;
     return (
       <View>
-        <Header onSideMenu={() => console.log()} title="Dashboard" />
-        <Text>TICKET BRO</Text>
+        <Header onSideMenu={() => console.log()} title="Dashboard"></Header>
+        <View style={styles.container}>
+          <View style={styles.fieldContainer}>
+            <Text style={[styles.label,{paddingLeft:5, paddingTop:5,paddingRight:5}]}>Feature</Text>
+            <View style={styles.featureDropdown}>
+              <Text style={[styles.label]}>Tickets</Text>
+                <Icon
+                  name='fontawesome|remove'
+                  size={15}
+                  color={'rgba(233, 114, 91, 0.5)'}
+                  style={{width: 15,height: 15,position:'absolute',top:4,right:10}}
+                />
+            </View>
+          </View>
+          <View style={styles.fieldContainer}>
+            <View style={[styles.featureDropdown,{marginRight:2}]}>
+              <Text style={[styles.label]}>Other</Text>
+                <Icon
+                  name='fontawesome|remove'
+                  size={15}
+                  color={'rgba(233, 114, 91, 0.5)'}
+                  style={{width: 15,height: 15,position:'absolute',top:4,right:10}}
+                />
+            </View>
+            <View style={[styles.featureDropdown,{marginRight:2}]}>
+              <Text style={[styles.label]}>Alessandro</Text>
+                <Icon
+                  name='fontawesome|remove'
+                  size={15}
+                  color={'rgba(233, 114, 91, 0.5)'}
+                  style={{width: 15,height: 15,position:'absolute',top:4,right:10}}
+                />
+            </View>
+          </View>
+
+          <View style={styles.fieldContainer}>
+            <View style={[styles.featureDropdown,{marginRight:2}]}>
+              <Text style={[styles.label]}>Inprogress</Text>
+                <Icon
+                  name='fontawesome|remove'
+                  size={15}
+                  color={'rgba(233, 114, 91, 0.5)'}
+                  style={{width: 15,height: 15,position:'absolute',top:4,right:10}}
+                />
+            </View>
+
+            <View style={[styles.container,{alignItems:'center',}]}>
+              <View style={[styles.sizingContainer]}>
+                <View style={styles.sizeContainer}>
+                  <Text style={[styles.ticketSize]}>?</Text>
+                </View>
+                <View style={styles.sizeContainer}>
+                  <Text style={[styles.ticketSize]}>S</Text>
+                </View>
+                <View style={styles.sizeContainer}>
+                  <Text style={[styles.ticketSize]}>M</Text>
+                </View>
+                <View style={[styles.sizeContainer,{backgroundColor:'#E69380'}]}>
+                  <Text style={[styles.ticketSize]}>L</Text>
+                </View>
+                <View style={[styles.sizeContainer,{marginRight:0}]}>
+                  <Text style={[styles.ticketSize]}>XL</Text>
+                </View>
+              </View>
+            </View>
+          </View>
+          <View style={styles.fieldContainer}>
+            <Text style={styles.ticketTitle}>#{ticket.local_id} {ticket.title}</Text>
+          </View>
+        </View>
       </View>
     );
   }
--- App/Styles/TicketView.js
+++ App/Styles/TicketView.js
@@ -8,4 +8,59 @@ var {
 
 
 module.exports = StyleSheet.create({
+  container: {
+    flex:1,
+  },
+  fieldContainer: {
+    flex:1,
+    flexDirection: 'row',
+    padding:5,
+    paddingTop:8,
+    paddingBottom:8,
+    borderBottomWidth:1,
+    borderColor:'#f2f2f2',
+  },
+  label: {
+    color: '#666',
+    fontFamily:'Proxima Nova',
+    fontSize: 14,
+    fontWeight: 'bold',
+    marginBottom: 2,
+  },
+  featureDropdown: {
+    position:'relative',
+    borderRadius:3,
+    padding:5,
+    borderWidth:1,
+    borderColor:'#dcdcdc',
+    flex:1,
+    fontWeight:'normal'
+  },
+
+  sizingContainer: {
+    flexDirection:'row'
+  },
+  sizeContainer: {
+    height:30,
+    backgroundColor:'#DCDCDC',
+    marginRight:2,
+    borderRadius:3,
+    paddingTop:8,
+    width:30,
+    alignItems:'center',
+    alignSelf:'center'
+  },
+  ticketSize: {
+    color:'#fff',
+    fontFamily:'Proxima Nova',
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
+  ticketTitle: {
+    color: '#4a4c5a',
+    fontFamily:'Proxima Nova',
+    fontSize: 14,
+    fontWeight: 'bold',
+    marginBottom: 2,
+  }
 });
